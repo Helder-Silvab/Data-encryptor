d426e8a3408fb58b1a3df1f0f8034430d44550d0
Helder Barbedo <8220234@estg.ipp.pt>
2023-04-19 18:38:30 +0100

    React

---
+++ React/codeShield/src/Login.jsx
@@ -0,0 +1,13 @@
+import { SignInButton } from "@clerk/clerk-react";
+import React from "react";
+
+
+function Login() {
+  return(
+    <div>
+        <SignInButton>Sign In</SignInButton>
+    </div>
+  )
+}
+
+export default Login;
---
+++ React/codeShield/src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Encriptar from "./Encriptar"
+import Login from "./Login"
+import "./utils/script.js"
+import "./styles/base.css"
+import "./styles/icons.css"
+import "./styles/style.css"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "encriptar",
+    element: <Encriptar />
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={"pk_test_cGxlYXNhbnQtbW9sbHVzay05NS5jbGVyay5hY2NvdW50cy5kZXYk"}>
+    <RouterProvider router={router} />
+  </ClerkProvider>
+)
